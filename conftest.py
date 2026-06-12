64a4da01329408b6074f4ad989c63d1f400661bf
Munazza <sqa.munazza@gmail.com>
2026-06-12 17:46:58 +0500

    fix: force headless mode when running in CI env

--- conftest.py
+++ conftest.py
@@ -1,3 +1,4 @@
+import os  # Added this to read the GitHub environment
 import logging
 import config
 import pytest
@@ -6,11 +7,21 @@ from playwright.sync_api import sync_playwright
 @pytest.fixture
 def page(): 
     logging.basicConfig(filename="automation.log", level=logging.INFO, format="%(asctime)s-%(levelname)s-%(message)s", force=True)
+    
+    # Check if running in GitHub Actions (CI). 
+    # If yes, force headless=True and slow_mo=0. Otherwise, use your config file settings.
+    is_headless = True if os.environ.get("CI") == "true" else config.HEADLESS
+    slow_down = 0 if os.environ.get("CI") == "true" else config.SLOW_MO
+
     with sync_playwright() as p:
-        browser=p.chromium.launch(headless=config.HEADLESS, slow_mo=config.SLOW_MO)
-        context=browser.new_context(record_video_dir=config.VIDEO_DIR)
-        page=context.new_page()
-        #page.set_default_timeout(config.TIME_OUT)
+        # Updated this line to use the smart variables
+        browser = p.chromium.launch(headless=is_headless, slow_mo=slow_down)
+        
+        context = browser.new_context(record_video_dir=config.VIDEO_DIR)
+        page = context.new_page()
+        # page.set_default_timeout(config.TIME_OUT)
+        
         yield page
         context.close()
         browser.close()
+        
